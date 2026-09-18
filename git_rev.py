797546cdc90fb7af1840817c6dd1893c528662a6
Pedro Santini <pedroantoniosantini@gmail.com>
2026-09-18 17:04:48 -0300

    fix(ci): support builds without git tags

--- git_rev.py
+++ git_rev.py
@@ -1,5 +1,5 @@
 import subprocess
 # Get 0.0.0 version from latest Git tag
-tagcmd = "git describe --tags"
+tagcmd = "git describe --tags --always"
 version = subprocess.check_output(tagcmd, shell=True).decode().strip()
 print(f'\'-D FIRMWAREVERSION="{version}"\'')
